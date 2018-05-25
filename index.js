f914563f4e164d444e884e8da7b7ecf38b6e6b87
fenrisl <daniel.deutsch25@gmail.com>
2018-05-25 14:51:54 +0200

    routes for sign up

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const express = require('express')
-const database = require('./src/database.js')
-// const routes = require("./routes") ???????
-const router = express.Router();
+const router = express.Router()
 const app = express()
-const bodyParser = require('body-parser');
+const database = require('./src/database.js')
+const bodyParser = require('body-parser')
 const avocatModel = require('./src/models/avocat.js')
-// const Avocat = require('./src/models/avocat.js')
-const auth = require('./routes/auth.js')
+const routes = require('./routes/routes.js')
 
 
 // MIDDLEWARES
@@ -28,28 +26,6 @@ app.get('/', (req, res) => {
 
 // new routes here..
 
-app.post('/reg', function (req, next) {
-
-  let newUser = new avocatModel({
-      email: req.body.email,
-      password: req.body.password,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      cabinet: req.body.cabinet,
-      phone: req.body.phone,
-      address: req.body.address,
-      zipCode: req.body.zipCode,
-      toque: req.body.toque,
-      field: req.body.field,
-  })
-  newUser.save()
-   .then(doc => {
-     console.log(doc)
-   })
-   .catch(err => {
-     console.error(err)
-   })
-  
-});
+app.use('/', routes)
 
 app.listen(3030, () => console.log(`server listening on port: 3030`))
---
+++ routes/routes.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const router = express.Router()
+const app = express()
+const avocatModel = require('../src/models/avocat.js')
+
+router.post('/reg', function (req, next) {
+
+    let newUser = new avocatModel({
+        email: req.body.email,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        cabinet: req.body.cabinet,
+        phone: req.body.phone,
+        address: req.body.address,
+        zipCode: req.body.zipCode,
+        toque: req.body.toque,
+        field: req.body.field,
+    })
+    newUser.save()
+     .then(doc => {
+       console.log(doc)
+     })
+     .catch(err => {
+       console.error(err)
+     })
+    
+  });
+
+  module.exports = router
+  
