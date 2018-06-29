7cc9f2a2e22632e25380226bbaf78f9809d58343
Baloulee <chinvalery@gmail.com>
2018-06-29 12:13:55 +0200

    route realisé but a problem to fix

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,46 @@
 const express = require('express')
 const router = express.Router()
-
 const AvocatModel = require('../models/avocat.js')
 const MissionModel = require('../models/mission.js')
 const StudentModel = require('../models/student.js')
 const bcrypt = require('bcrypt-promise')
 const jwt = require('jsonwebtoken')
 const jwtSecret = 'MAKEITUNUVERSAL'
+
+const bodyParser = require('body-parser')
+const multer = require('multer')
+const uuidv4 = require('uuid/v4')
+const path = require('path')
+
+// configure storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    /*
+      Files will be saved in the 'uploads' directory. Make
+      sure this directory already exists!
+    */
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+    /*
+      uuidv4() will generate a random ID that we'll use for the
+      new filename. We use path.extname() to get
+      the extension from the original file name and add that to the new
+      generated ID. These combined will create the file name used
+      to save the file on the server and will be available as
+      req.file.pathname in the router handler.
+    */
+    const newFilename = `${uuidv4()}${path.extname(file.originalname)}`
+    cb(null, newFilename)
+  },
+})
+// create the multer instance that will be used to upload/save the file
+const upload = multer({ storage })
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // POST Registration Student
 
@@ -112,6 +146,17 @@ router.get('/missions', (req, res, next) => {
   MissionModel.find()
     .then(missions => res.json(missions.filter(mission => mission.finished === false)))
     .catch(next)
+})
+
+// POST Upload file
+router.post('/missions/:missionId', upload.single('selectedFile'), (req, res) => {
+  /*
+    We now have a new req.file object here. At this point the file has been saved
+    and the req.file.filename value will be the name returned by the
+    filename() function defined in the diskStorage configuration. Other form fields
+    are available here in req.body.
+  */
+  res.send()
 })
 
 // GET ONE CURRENT MISSION
