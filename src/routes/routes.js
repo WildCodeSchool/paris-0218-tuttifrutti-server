efddbf20fbfe3be9dc851f98f6fd25fc96a164f0
Baloulee <chinvalery@gmail.com>
2018-07-02 11:57:49 +0200

    routes faites pour l'upload coté server

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,34 +13,56 @@ const uuidv4 = require('uuid/v4')
 const path = require('path')
 
 // configure storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    /*
-      Files will be saved in the 'uploads' directory. Make
-      sure this directory already exists!
-    */
-    cb(null, './uploads');
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     /*
+//       Files will be saved in the 'uploads' directory. Make
+//       sure this directory already exists!
+//     */
+//     cb(null, 'uploads');
+//   },
+//   filename: (req, file, cb) => {
+//     /*
+//       uuidv4() will generate a random ID that we'll use for the
+//       new filename. We use path.extname() to get
+//       the extension from the original file name and add that to the new
+//       generated ID. These combined will create the file name used
+//       to save the file on the server and will be available as
+//       req.file.pathname in the router handler.
+//     */
+//     const newFilename = `${uuidv4()}${path.extname(file.originalname)}`
+//     cb(null, newFilename)
+//   },
+//})
+
+
+ const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'tmp/')
   },
-  filename: (req, file, cb) => {
-    /*
-      uuidv4() will generate a random ID that we'll use for the
-      new filename. We use path.extname() to get
-      the extension from the original file name and add that to the new
-      generated ID. These combined will create the file name used
-      to save the file on the server and will be available as
-      req.file.pathname in the router handler.
-    */
-    const newFilename = `${uuidv4()}${path.extname(file.originalname)}`
-    cb(null, newFilename)
-  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
 })
+
+const upload = multer({ storage: storage })
+
 // create the multer instance that will be used to upload/save the file
-const upload = multer({ storage })
 
-const app = express()
+router.use(bodyParser.json())
+router.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+
+// Upload  de fichier
+router.post('/upload', upload.single('selectedFile'), (req, res) => {
+  /*
+    We now have a new req.file object here. At this point the file has been saved
+    and the req.file.filename value will be the name returned by the
+    filename() function defined in the diskStorage configuration. Other form fields
+    are available here in req.body.
+  */
+  res.send()
+})
 
 // POST Registration Student
 
