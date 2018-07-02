9c78fa044476e8f4f8b4aa6546235a32ef9d1ebd
Baloulee <chinvalery@gmail.com>
2018-07-02 23:08:04 +0200

    error limit file size

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,31 +12,7 @@ const multer = require('multer')
 const uuidv4 = require('uuid/v4')
 const path = require('path')
 
-// configure storage
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     /*
-//       Files will be saved in the 'uploads' directory. Make
-//       sure this directory already exists!
-//     */
-//     cb(null, 'uploads');
-//   },
-//   filename: (req, file, cb) => {
-//     /*
-//       uuidv4() will generate a random ID that we'll use for the
-//       new filename. We use path.extname() to get
-//       the extension from the original file name and add that to the new
-//       generated ID. These combined will create the file name used
-//       to save the file on the server and will be available as
-//       req.file.pathname in the router handler.
-//     */
-//     const newFilename = `${uuidv4()}${path.extname(file.originalname)}`
-//     cb(null, newFilename)
-//   },
-//})
-
-
- const storage = multer.diskStorage({
+const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, 'tmp/')
   },
@@ -45,23 +21,28 @@ const path = require('path')
   }
 })
 
-const upload = multer({ storage: storage })
+const upload = multer({
+  storage: storage,
+  limits: { fieldSize: 1024 }
+})
 
 // create the multer instance that will be used to upload/save the file
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }))
 
-
 // Upload  de fichier
 router.post('/upload', upload.single('selectedFile'), (req, res) => {
-  /*
-    We now have a new req.file object here. At this point the file has been saved
-    and the req.file.filename value will be the name returned by the
-    filename() function defined in the diskStorage configuration. Other form fields
-    are available here in req.body.
-  */
-  res.send()
+  console.log(res)
+  res.send({ result: 'ok' })
+})
+
+// Handle any other errors
+router.use(function (err, req, res, next) {
+  if (err.code === 'LIMIT_FILE_SIZE') {
+    res.send({ result: 'fail', error: { code: 1001, message: 'File is too big' } })
+    return
+  }
 })
 
 // POST Registration Student
@@ -173,10 +154,10 @@ router.put('/infolawyer', async (req, res, next) => {
       toque: update.toque,
       field: update.field
     }
-  })
-    .then(lawyer => console.log(lawyer))
-    .then(lawyer => res.json(lawyer))
-    .catch(next)
+})
+      .then(lawyer => console.log(lawyer))
+      .then(lawyer => res.json(lawyer))
+      .catch(next)
   }
 })
 
@@ -198,12 +179,7 @@ router.get('/missions', (req, res, next) => {
 
 // POST Upload file
 router.post('/missions/:missionId', upload.single('selectedFile'), (req, res) => {
-  /*
-    We now have a new req.file object here. At this point the file has been saved
-    and the req.file.filename value will be the name returned by the
-    filename() function defined in the diskStorage configuration. Other form fields
-    are available here in req.body.
-  */
+  console.log('blabla', req)
   res.send()
 })
 
